0ec790e3cb58368ebfb0f67bf2dff92d146257d5
John <john.t.singer@gmail.com>
2023-08-03 14:45:46 +0200

    Add requirements and some change

--- resize.py
+++ resize.py
@@ -9,7 +9,6 @@ class ResizeImage:
     """Classe pour redimentionner une image"""
 
     def __init__(self, path, new_path=None, height=None, width=None):
-
         """
         Constructeur de la classe
         :param path: str, le path de l'image
@@ -27,17 +26,16 @@ class ResizeImage:
         self.new_size = (None, None)
 
     def open_image(self):
-
         """Ouvre l'image"""
 
         try:
             self.image = Image.open(self.path)
         except FileNotFoundError:
-            print(f"FileNotFoundError: No such file or directory:\n '{self.path}'")
+            print(
+                f"FileNotFoundError: No such file or directory:\n '{self.path}'")
             sys.exit(1)
 
     def calculate_ratio(self):
-
         """Calcule le ratio"""
 
         if self.width is not None:
@@ -46,30 +44,28 @@ class ResizeImage:
             self.ratio = self.height / float(self.image.size[1])
 
     def height_size(self):
-
         """Calcule la hauteur si on a donner la largeur"""
 
         self.hsize = int((float(self.image.size[1]) * float(self.ratio)))
 
     def width_size(self):
-
         """Calcule la largeur si on a donner la hauteur"""
 
         self.wsize = int((float(self.image.size[0]) * float(self.ratio)))
 
     def img_resize(self):
-
         """Redimentionne l'image"""
 
         if self.width:
             self.height_size()
-            self.image = self.image.resize((self.width, self.hsize), Image.ANTIALIAS)
+            self.image = self.image.resize(
+                (self.width, self.hsize), Image.ANTIALIAS)
         else:
             self.width_size()
-            self.image = self.image.resize((self.wsize, self.height), Image.ANTIALIAS)
+            self.image = self.image.resize(
+                (self.wsize, self.height), Image.ANTIALIAS)
 
     def save(self):
-
         """Enregistre l'image redimmentionée en rajoutant 'resized' au nom
         de l'image
         Si self.new_path n'est pas définie enregistre l'image au même endroit
@@ -88,12 +84,12 @@ class ResizeImage:
         self.image.save(path)
 
     def auto_resizing(self):
-
         """Auto redimentionnage"""
 
         self.open_image()
         if self.width and self.height:
-            self.image = self.image.resize((self.width, self.height), Image.ANTIALIAS)
+            self.image = self.image.resize(
+                (self.width, self.height), Image.ANTIALIAS)
         else:
             self.calculate_ratio()
             self.img_resize()
@@ -105,7 +101,6 @@ class ResizeImage:
 if __name__ == '__main__':
 
     def main():
-
         """Programe pricipal lors de l'execution du script"""
 
         args = parse_argument()
@@ -124,14 +119,14 @@ if __name__ == '__main__':
                 width = int(width)
             if height:
                 height = int(height)
-            resize = ResizeImage(path, new_path=new_path, width=width, height=height)
+            resize = ResizeImage(path, new_path=new_path,
+                                 width=width, height=height)
             resize.auto_resizing()
             if args.verbose:
                 new_size = resize.new_size
                 verbosity(args, new_size)
 
     def verbosity(args, new_size):
-
         """Affiche les informations du redimentionnage"""
 
         if args.verbose:
@@ -147,18 +142,17 @@ if __name__ == '__main__':
     Located at : {path}
     To : {new_name}
     Located at : {new_path}
-    With new size : 
+    With new size :
         width   {width} px
         height  {height} px"""
             print(text)
 
     def parse_argument():
-
         """
         Créer les arguments
         :return: les arguments parser
         """
-        
+
         parser = argparse.ArgumentParser()
         parser.add_argument("-p", "--path", help="path to the file")
         parser.add_argument("-np", "--newpath", help="new path to the file")
